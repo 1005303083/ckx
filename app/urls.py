d730eb75c6ae84e5c1bbf84ad329c58691453fa6
1005303083 <1005303083@qq.com>
2019-03-11 09:13:04 +0800

    axf

---
+++ app/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from app import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),  # 首页
+    url(r'^market/$', views.market, name='marketbase'), # 闪购超市
+    url(r'^market/(?P<childid>\d+)/(?P<sortid>\d+)/$', views.market, name='market'), # 闪购超市
+
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^mine/$', views.mine, name='mine'),
+
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+]
